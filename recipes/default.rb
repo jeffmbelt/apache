ebb4507f32e3eb61b57c40005df1406a3be58b39
Jeffrey Belt <jeffmbelt@gmail.com>
2020-01-27 19:52:29 +0000

    New Cookbook for Apache

---
+++ recipes/default.rb
@@ -0,0 +1,18 @@
+apt_update
+
+package 'net-tools' do
+  action :install
+end
+
+package 'apache2' do
+  action :install
+end
+
+file '/var/www/html/index.html' do
+  action :create
+  content 'Hello Pipeline World!'
+end
+
+service 'apache2' do
+  action [ :enable, :start ]
+end
