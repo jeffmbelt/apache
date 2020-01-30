0577ab231c8396e29c1db8e0a86bb21dec2d8b4c
Jeffrey Belt <jeffmbelt@gmail.com>
2020-01-30 20:09:47 +0000

    .

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,16 +8,20 @@ package 'apache2' do
   action :install
 end
 
-file '/var/www/html/index.html' do
-  action :create
-  content "<html>
-<body>
-<h1>Hello Pipeline World!</h1>
-<h2>This machine is running #{node['os']}</h2>
-<h2>My ip address is #{node['cloud']['public_ipv4']}</h2>
-<h2>This computer belongs to #{node['apache']['company_name']}</h2>
-</body>
-</html>"
+# file '/var/www/html/index.html' do
+#   action :create
+#   content "<html>
+# <body>
+# <h1>Hello Pipeline World!</h1>
+# <h2>This machine is running #{node['os']}</h2>
+# <h2>My ip address is #{node['cloud']['public_ipv4']}</h2>
+# <h2>This computer belongs to #{node['apache']['company_name']}</h2>
+# </body>
+# </html>"
+# end
+
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
 end
 
 service 'apache2' do
